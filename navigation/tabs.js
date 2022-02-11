5d9f55006ad4b8737b8a3912b6ea9023ec70cab1
csa <csa@voxteneo.com>
2022-02-11 18:34:25 +0700

    initial commit

---
+++ navigation/tabs.js
@@ -0,0 +1,37 @@
+import React from "react";
+import {
+    View,
+} from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+
+import { Home } from "../screens"
+
+const Tab = createBottomTabNavigator()
+
+const Tabs = () => {
+    return (
+        <Tab.Navigator
+            screenOptions={{
+                headerShown: false,
+            }}>
+            <Tab.Screen
+                name="Home"
+                component={Home}
+            />
+            <Tab.Screen
+                name="Search"
+                component={Home}
+            />
+            <Tab.Screen
+                name="Bookmark"
+                component={Home}
+            />
+            <Tab.Screen
+                name="Settings"
+                component={Home}
+            />
+        </Tab.Navigator>
+    )
+}
+
+export default Tabs;
